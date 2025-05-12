4217bf18d8e45fd000ea7febc7d1c4389853d9a1
jogimo26 <jogimo26@gmail.com>
2025-05-12 07:41:40 -0500

    Update NuevaVentana.py

--- Servidor/NuevaVentana.py
+++ Servidor/NuevaVentana.py
@@ -2,69 +2,82 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from new_window import NewWindow
-from parking_client import *
+from parking_client import registerUser
 
 class MainWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
-
-        self.setWindowTitle("Test new window")
-       
+        self.setWindowTitle("Registro")
         # Se crean los labels
-        l1=QLabel('ID: ')
-        l2=QLabel('Contraseña: ')
+        l1 = QLabel('ID: ')
+        l2 = QLabel('Contraseña: ')
+        l3 = QLabel('Programa: ')
+        l4 = QLabel('Rol: ')
 
         # Se crean los campos de entrada
-        self.e1=QLineEdit()
-        self.e2=QLineEdit()
+        self.e1 = QLineEdit()
+        self.e2 = QLineEdit()
+        self.e3 = QLineEdit()
+        self.e4 = QLineEdit()
 
-        b1=QPushButton('Obtener QR')
+        b1=QPushButton('Registrar Usuario')
         b1.clicked.connect(self.newWindow)
         
         
         gridLayout=QGridLayout()
         
-         # Se añaden los widgets al layout
+        # Se añaden los widgets al layout
+        # Textos
         gridLayout.addWidget(l1,0,0)
         gridLayout.addWidget(l2,1,0)
+        gridLayout.addWidget(l3,2,0)
+        gridLayout.addWidget(l4,3,0)
         
+        #Entradas de texto
         gridLayout.addWidget(self.e1,0,1)
         gridLayout.addWidget(self.e2,1,1)
+        gridLayout.addWidget(self.e3,2,1)
+        gridLayout.addWidget(self.e4,3,1)
 
-        gridLayout.addWidget(b1,2,0,1,2)
+        # Botón del registro
+        gridLayout.addWidget(b1,4,0,1,2)
         
-
-
         widget = QWidget()
         widget.setLayout(gridLayout)
+
         #QMainWindow requiere un widget central
         self.setCentralWidget(widget)
-
         
         # Deshabilita el botón de maximizar
-        self.setWindowFlags( Qt.MSWindowsFixedSizeDialogHint)
+        self.setWindowFlags(Qt.MSWindowsFixedSizeDialogHint)
     
     def newWindow(self):
-        id=self.e1.text()
-        password=self.e2.text()
-        if len(id) and len(password):
+        id = self.e1.text()
+        password = self.e2.text()
+        program = self.e3.text()
+        role = self.e4.text()
+
+        if len(id) and len(password) and len(program) and len(role):
             url="http://localhost:80"
 
-            # Solicita un código QR al servidor (los códigos QR cambian cada fecha o cuando se reinicia el servidor)
-            imgBytes=getQR(url,id,password)
-            if len(imgBytes):
-                self.nw=NewWindow(imgBytes)
-                self.nw.show()
+            # Registra el usuario
+            Usuario = registerUser(url, id, password, program, role)
+            if Usuario == "User successfully registered":
+                msgBox = QMessageBox()
+                msgBox.setIcon(QMessageBox.Information)
+                msgBox.setText("Usuario registrado con éxito")
+                msgBox.setWindowTitle("Información")
+                msgBox.setStandardButtons(QMessageBox.Ok)
+                msgBox.exec()
             else:
                 msgBox = QMessageBox()
                 msgBox.setIcon(QMessageBox.Warning)
-                msgBox.setText("Usuario no Existe o Contraseña Incorrecta")
+                msgBox.setText("Usuario ya existente dentro del sistema")
                 msgBox.setWindowTitle("Alerta")
                 msgBox.setStandardButtons(QMessageBox.Ok)
                 msgBox.exec()
 
-       
 app = QApplication([])
 ex = MainWindow()
 ex.show()
